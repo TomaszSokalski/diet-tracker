a93d81b56b4fccc2dfedce0d771bb5b111b18ae7
TomaszSokalski <tsokalski88@gmail.com>
2022-12-29 20:57:39 +0100

    Adding button to show and remove food

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FoodListService } from './food-list.service';
+import { Food } from './food.interface';
+import { finalize, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,12 +11,27 @@ import { FoodListService } from './food-list.service';
 export class AppComponent {
   response$ = this.foodService.foods;
   title = 'diet-trucker';
+  selectedFood$: Observable<Food>;
+  isLoading = false;
 
   constructor (private foodService: FoodListService) {}
 
   getTitle (): string {
     return this.title; 
   }
-};
+
+  onDetails (id: number) {
+    this.selectedFood$ = this.foodService.onGetItem(id)
+  }
+
+  onDelete (id: number) {
+    this.isLoading = true;
+    this.foodService.onDelete(id).pipe(finalize(() => {
+      this.isLoading = false
+    })).subscribe(() => {
+      this.response$ = this.foodService.foods;
+    })
+  }
+}
 
 
--- src/app/food-list.service.ts
+++ src/app/food-list.service.ts
@@ -1,13 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Response } from './response.interface';
+import { Food } from './food.interface'
 
 @Injectable()
 export class FoodListService {
 
   constructor(private httpClient: HttpClient) { }
-  
+
+  private API_URL = 'http://localhost:8080/api';
+
  get foods()  {
-    return this.httpClient.get<Response>(`http://localhost:8080/foods`);
+    return this.httpClient.get<Response>(`${this.API_URL}/foods`);
+  }
+
+  onDelete(id: number) {
+    return this.httpClient.delete(`${this.API_URL}/foods/${id}`)
+  }
+
+  onGetItem(id: number) {
+    return this.httpClient.get<Food>(`${this.API_URL}/foods/${id}`)
   }
 }
+
