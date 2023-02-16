d8f689e7c05511c590582d4c8d80dfb0f8a2d442
TomaszSokalski <tsokalski88@gmail.com>
2023-02-16 23:09:22 +0100

    add pretty loader #13

--- src/app/components/loader/loader.component.ts
+++ src/app/components/loader/loader.component.ts
@@ -1,10 +1,10 @@
-import { Component, Input} from '@angular/core';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-loader',
   templateUrl: './loader.component.html',
-  styleUrls: ['./loader.component.scss']
+  styleUrls: ['./loader.component.scss'],
 })
 export class LoaderComponent {
-  @Input() isLoading = false;
 }
