c93eaa2f94887b685e894cecd404664d89505b45
TomaszSokalski <tsokalski88@gmail.com>
2023-02-24 12:16:52 +0100

    diary component as wrapper

--- src/app/views/diary/diary.component.ts
+++ src/app/views/diary/diary.component.ts
@@ -1,19 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { DiaryService } from './services/diary.service';
-import { DiaryState } from './state/diary.state';
-
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-diary',
   templateUrl: './diary.component.html',
   styleUrls: ['./diary.component.scss'],
 })
-export class DiaryComponent implements OnInit {
-  diary$ = this.diaryState.diary$;
-
-  constructor(private diaryState: DiaryState) {}
-
-  ngOnInit(): void {
-    this.diaryState.getDiary();
-  }
-}
+export class DiaryComponent {}
