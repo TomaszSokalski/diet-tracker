6dc62e556919aee25c0add6beb80a322fd2e7039
TomaszSokalski <tsokalski88@gmail.com>
2023-03-06 10:42:56 +0100

    add and rebuild interfaces

--- src/app/interfaces/food.interface.ts
+++ src/app/interfaces/food.interface.ts
@@ -1,7 +1,11 @@
+import { NutriScore } from "../shared/components/nutri-score/nutri-score.enum";
+import { Tag } from "./tag.interface";
+
 export interface Food {
-    id: string;
-    name: string;
-    weight: number;
-    caloriesPer100g?: number;
-    nutriScore?: string;
-  };
+  id: string;
+  name?: string;
+  caloriesPer100g?: number;
+  weight: number;
+  nutriScore?: NutriScore;
+  tags?: Tag['id'][];
+};
