56e36d4920c1b22a80d8b0c935b843b9abf6c420
TomaszSokalski <tsokalski88@gmail.com>
2023-03-06 11:06:22 +0100

    add unsubscribe

--- src/app/views/diary/components/diary-table/diary-table.component.ts
+++ src/app/views/diary/components/diary-table/diary-table.component.ts
@@ -1,8 +1,10 @@
 import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { filter } from 'rxjs';
+import { filter, Subject, takeUntil } from 'rxjs';
 import { Diary } from 'src/app/interfaces/diary.interface';
+import { FoodnamePipe } from 'src/app/shared/pipes/foodname.pipe';
+import { FoodListState } from 'src/app/views/foods-list/state/food-list.state';
 
 import { DISPLAYED_COLUMNS } from '../../displayed-columns.const';
 import { DiaryState } from '../../state/diary.state';
@@ -21,11 +23,23 @@ export class DiaryTableComponent implements OnInit {
   loading$ = this.diaryState.loading$;
   error$ = this.diaryState.error$;
 
-  constructor(private diaryState: DiaryState, private datePipe: DatePipe) {}
+  private destroy$ = new Subject<void>();
+
+  constructor(
+    private diaryState: DiaryState,
+    private foodListState: FoodListState,
+    private datePipe: DatePipe,
+    private snackbar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {
     this.getInitialValue();
     this.listenToErrors();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.unsubscribe();
   }
 
   deleteFoodInDiary(diary: Diary): void {
@@ -38,9 +52,13 @@ export class DiaryTableComponent implements OnInit {
   }
 
   private listenToErrors() {
-    this.error$.pipe(filter((v) => v !== null)).subscribe((err) => {
-      this.snackbar.open(err?.message ?? 'Error');
-    });
+    this.error$
+      .pipe(
+        takeUntil(this.destroy$),
+        filter((v) => v !== null)
+      )
+      .subscribe((err) => {
+        this.snackbar.open(err?.message ?? 'Error');
+      });
   }
-  // update take until
 }
