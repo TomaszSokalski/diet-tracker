243e24fc02f78a678710e1376a47452a5c6361fb
TomaszSokalski <tsokalski88@gmail.com>
2023-03-08 22:37:59 +0100

    Adding SEO Title and Meta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 
 @Component({
@@ -6,6 +7,22 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
+  constructor(private metaTagService: Meta, private titleService: Title) {}
+
+  ngOnInit(): void {
+    this.metaTagService.addTags([
+      {
+        name: 'keywords',
+        content: 'Angular SEO Integration, foods, diary, lifestyle',
+      },
+      { name: 'robots', content: 'index' },
+      { name: 'author', content: 'Tomasz Sokalski' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'date', content: '2023-03-08', scheme: 'YYYY-MM-DD' },
+      { charset: 'UTF-8' },
+    ]);
+
+    this.titleService.setTitle('Diet Tracker')
+  }
 }
