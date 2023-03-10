0d40a41fef52a8bfea7d941d488a766935c909ab
TomaszSokalski <tsokalski88@gmail.com>
2023-03-10 09:55:21 +0100

    rename unsubscribe component

---
+++ src/app/components/unsubscribe.ts
@@ -0,0 +1,12 @@
+import { Injectable, OnDestroy } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Injectable()
+export abstract class UnsubscribeComponent implements OnDestroy {
+  destroy$ = new Subject<void>();
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
