f4f0200fc4cb81330fc9abc905f405a77c6bb7e3
TomaszSokalski <tsokalski88@gmail.com>
2023-04-19 17:32:07 +0200

    add login component

--- src/app/shared/auth/components/login/login.component.ts
+++ src/app/shared/auth/components/login/login.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { combineLatest } from 'rxjs';
 import { AuthState } from '../state/auth.state';
-import { Login } from './login.interface';
 
 @Component({
   selector: 'app-login',
@@ -20,50 +18,27 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
     this.initForm();
-    combineLatest([
-      this.password?.valueChanges,
-      this.repeatedPassword?.valueChanges,
-    ]).subscribe((value) => {
-      console.log(value);
-      // if (value[0] === value[1]) {
-      // }
-    });
   }
 
   get password() {
     return this.form.get('password');
   }
 
-  get repeatedPassword() {
-    return this.form.get('repeatedPassword');
+  get name() {
+    return this.form.get('name')
   }
 
   onLoginClicked(): void {
     if (this.form.invalid) {
-      console.log('U are not allowed to');
+      return;
     }
-    const payload = this.loginPayload(this.form);
-    this.authState.postUser(payload);
+    this.authState.postUser(this.form.value);
   }
 
   private initForm(): void {
     this.form = this.fb.group({
       name: [null, [Validators.required]],
-      email: [null, [Validators.email, Validators.required]],
-      password: [null, [Validators.minLength(this.minPw), Validators.required]],
-      repeatedPassword: [
-        null,
-        [Validators.minLength(this.minPw), Validators.required],
-      ],
+      password: [null, [Validators.required, Validators.minLength(6)]],
     });
-  }
-
-  private loginPayload(form: FormGroup): Login {
-    const { value } = form;
-    return {
-      name: value.name,
-      email: value.email,
-      password: value.password,
-    };
   }
 }
--- src/app/shared/auth/components/login/login.interface.ts
+++ src/app/shared/auth/components/login/login.interface.ts
@@ -1,5 +1,4 @@
 export interface Login {
   name: string;
-  email: string;
   password: string;
 }
